ddcfb84697879449ab281d5d821e0832ea4a12bb
lislis <mail@lislis.de>
2016-11-28 18:47:10 +0100

    add faq toggle

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
 
 document.addEventListener("DOMContentLoaded", function () {
+
+  document.querySelector('body').classList.add('has-js');
+
+  // tofuburger toggle
+  
   var burger = document.querySelector('.logo-burger');
   var cross = document.querySelector('.navigation-button');
   var nav = document.querySelector('.l-navigation');
@@ -10,5 +15,23 @@ document.addEventListener("DOMContentLoaded", function () {
   cross.addEventListener('click', function () {
     nav.classList.toggle('is-open');
   });
+
+  // faq accordion
+
+  if (document.querySelector('body').classList.contains('has-js')) {
+    var heads = document.querySelectorAll('.accordion-h');
+    var bodies = document.querySelectorAll('.accordion-b');
+
+    for (var item of bodies) {
+      item.classList.add('is-hidden');
+    }
+
+    for (var i = 0; i < heads.length; i++) {
+      heads[i].addEventListener('click', function (el) {
+        this.classList.toggle('is-open');
+        this.nextElementSibling.classList.toggle('is-hidden');
+      });
+    }
+  }
 });
 
