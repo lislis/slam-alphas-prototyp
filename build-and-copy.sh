f2c3771829726ecce60651dc8520de5c0a8ee1f5
lislis <lislis@users.noreply.github.com>
2017-08-21 20:06:20 +0200

    Delete build-and-copy.sh

--- build-and-copy.sh
+++
@@ -1,8 +0,0 @@
-jekyll build; 
-echo 'finished building';
-cp ~/projects/slam-alphas-proto/_site/css/main.css /Applications/MAMP/htdocs/wordpress/wp-content/themes/slamalphas/css/main.css
-cp ~/projects/slam-alphas-proto/_site/js/main.js /Applications/MAMP/htdocs/wordpress/wp-content/themes/slamalphas/js/main.js
-echo 'finished coyiing';
-echo 'starting upload';
-scp -r /Applications/MAMP/htdocs/wordpress/wp-content/themes/slamalphas/ alphas@shaula.uberspace.de:~/html/wp-content/themes/slamalphas;
-echo 'finished upload';
